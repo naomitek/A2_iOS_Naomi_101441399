e36cf7416a937a47db63539c68db1d0aa6a4098e
Naomi Teklu <101441399@georgebrown.ca>
2025-03-28 22:11:00 -0400

    Update ContentView code

---
+++ A2_iOS_Naomi_101441399 9.46.28 PM/A2_iOS_Naomi_101441399/ProductDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  ProductDetailView.swift
+//  A2_iOS_Naomi_101441399
+//
+//  Created by usr on 2025-03-28.
+//
+
+import Foundation
--- A2_iOS_Naomi_101441399/ContentView.swift
+++ A2_iOS_Naomi_101441399/ContentView.swift
@@ -10,77 +10,188 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Product.timestamp, ascending: true)], animation: .default)
+    private var products: FetchedResults<Product>
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    @State private var selectedProduct: Product?
+    @State private var searchText = ""
+    @State private var isAddingProduct = false
+    @State private var showProductList = false
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    @State private var isEditingProduct = false
+
+    let backgroundColor = Color(red: 0.6, green: 0.8, blue: 1.0)
+    let deleteButtonColor = Color.red
+    let editButtonColor = Color.white
+
+    var filteredProducts: [Product] {
+        if searchText.isEmpty {
+            return Array(products)
+        } else {
+            return products.filter { product in
+                product.name?.localizedCaseInsensitiveContains(searchText) ?? false ||
+                product.productDescription?.localizedCaseInsensitiveContains(searchText) ?? false
+            }
+        }
+    }
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack {
+                if let product = selectedProduct {
+                    VStack {
+                        ProductDetailView(product: product)
+                        HStack {
+                            Button(action: {
+                                deleteProduct(product: product)
+                            }) {
+                                Text("Delete")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(deleteButtonColor)
+                                    .cornerRadius(8)
+                            }
+
+                            Button(action: {
+                                isEditingProduct = true
+                            }) {
+                                Text("Edit")
+                                    .foregroundColor(.black)
+                                    .padding()
+                                    .background(editButtonColor)
+                                    .cornerRadius(8)
+                            }
+                        }
+                    }
+                } else if !filteredProducts.isEmpty {
+                    VStack {
+                        ProductDetailView(product: filteredProducts.first!)
+                        HStack {
+                            Button(action: {
+                                deleteProduct(product: filteredProducts.first!)
+                            }) {
+                                Text("Delete")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(deleteButtonColor)
+                                    .cornerRadius(8)
+                            }
+
+                            Button(action: {
+                                isEditingProduct = true
+                            }) {
+                                Text("Edit")
+                                    .foregroundColor(.black)
+                                    .padding()
+                                    .background(editButtonColor)
+                                    .cornerRadius(8)
+                            }
+                        }
+                    }
+                } else {
+                    Text("No products found.")
+                        .foregroundColor(.gray)
+                }
+
+                HStack {
+                    if !filteredProducts.isEmpty {
+                        Button(action: {
+                            if let currentIndex = filteredProducts.firstIndex(of: selectedProduct ?? filteredProducts.first!), currentIndex > 0 {
+                                selectedProduct = filteredProducts[currentIndex - 1]
+                            } else {
+                                selectedProduct = filteredProducts.last!
+                            }
+                        }) {
+                            Image(systemName: "chevron.left")
+                                .foregroundColor(.white) // Chevron colors
+                        }
+                        .disabled(filteredProducts.firstIndex(of: selectedProduct ?? filteredProducts.first!) == 0)
+
+                        Spacer()
+
+                        Button(action: {
+                            if let currentIndex = filteredProducts.firstIndex(of: selectedProduct ?? filteredProducts.first!), currentIndex < filteredProducts.count - 1 {
+                                selectedProduct = filteredProducts[currentIndex + 1]
+                            } else {
+                                selectedProduct = filteredProducts.first!
+                            }
+                        }) {
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.white) // Chevron colors
+                        }
+                        .disabled(filteredProducts.firstIndex(of: selectedProduct ?? filteredProducts.first!) == filteredProducts.count - 1)
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                .padding()
+
+                HStack {
+                    TextField(products.isEmpty ? "Not Available" : "Search Products", text: $searchText)
+                        .padding(8)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .disabled(products.isEmpty)
+                        .foregroundColor(products.isEmpty ? .gray : .primary)
+                        .onChange(of: searchText) { _ in
+                            selectedProduct = filteredProducts.first
+                        }
+
+                    Button(action: {
+                        showProductList = true
+                    }) {
+                        Image(systemName: "list.bullet")
+                            .foregroundColor(.black)
                     }
+                    .padding(.leading, 8)
+                    .disabled(products.isEmpty)
+
+                    Button(action: {
+                        isAddingProduct = true
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.system(size: 30))
+                            .foregroundColor(.black)
+                    }
+                    .padding(.leading, 8)
+                }
+                .padding()
+            }
+            .padding(.bottom)
+            .navigationTitle("Products")
+            .sheet(isPresented: $isAddingProduct) {
+                AddProductView()
+            }
+            .sheet(isPresented: $showProductList) {
+                ProductListView(products: filteredProducts)
+            }
+            .sheet(isPresented: $isEditingProduct) {
+                if let product = selectedProduct {
+                    EditProductView(product: Binding(
+                        get: { product },
+                        set: { selectedProduct = $0 }
+                    ))
                 }
             }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            .onAppear {
+                if let firstProduct = filteredProducts.first {
+                    selectedProduct = firstProduct
+                }
+            }
+            .background(backgroundColor.edgesIgnoringSafeArea(.all)) // Apply flashy, fantastic light blue
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             }
         }
     }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    private func deleteProduct(product: Product) {
+        viewContext.delete(product)
+        do {
+            try viewContext.save()
+        } catch {
+            print("Failed to delete product: \(error)")
+            alertMessage = "Failed to delete product. Please try again."
+            showingAlert = true
         }
     }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
