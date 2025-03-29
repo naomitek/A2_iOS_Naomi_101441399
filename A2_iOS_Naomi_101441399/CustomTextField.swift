e8efa4b306b8bb9e484ae7006d9b0812f475a110
Naomi Teklu <101441399@georgebrown.ca>
2025-03-28 23:55:00 -0400

    Update CustomTextField

--- A2_iOS_Naomi_101441399/CustomTextField.swift
+++ A2_iOS_Naomi_101441399/CustomTextField.swift
@@ -1,9 +1,10 @@
 //
-//   CustomTextField.swift
+//  CustomTextField.swift
 //  A2_iOS_Naomi_101441399
 //
 //  Created by usr on 2025-03-28.
 //
+
 import SwiftUI
 struct CustomTextField: View {
     var title: String
